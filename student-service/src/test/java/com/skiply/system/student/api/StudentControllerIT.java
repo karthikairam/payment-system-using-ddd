d1450e942dae943497e06858c91f43d2ea84cf92
Karthikaiselvan <karthikairam@gmail.com>
2023-01-09 18:44:03 +0400

    Integration Test for Student registration - negative case

--- student-service/src/test/java/com/skiply/system/student/api/StudentControllerIT.java
+++ student-service/src/test/java/com/skiply/system/student/api/StudentControllerIT.java
@@ -6,6 +6,7 @@ import com.skiply.system.common.domain.model.valueobject.StudentId;
 import com.skiply.system.student.api.register.RegisterStudentCommand;
 import com.skiply.system.student.infrastructure.persistence.repository.StudentRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,11 @@ class StudentControllerIT {
     @Autowired
     private StudentRepository repository;
 
+    @AfterEach
+    void cleanUp() {
+        repository.deleteAll();
+    }
+
     @Test
     void givenValidRegistrationRequestThenSystemRegistersTheStudent() throws Exception {
         var request = RegisterStudentCommand.builder()
@@ -53,7 +59,6 @@ class StudentControllerIT {
         ;
 
         var entity = repository.findById(request.studentId().value());
-        Assertions.assertThat(entity.isPresent()).isTrue();
         entity.ifPresentOrElse(studentEntity -> {
             Assertions.assertThat(studentEntity.getStudentId()).isEqualTo(request.studentId().value());
             Assertions.assertThat(studentEntity.getStudentName()).isEqualTo(request.studentName());
@@ -61,6 +66,65 @@ class StudentControllerIT {
             Assertions.assertThat(studentEntity.getMobileNumber()).isEqualTo(request.mobileNumber());
             Assertions.assertThat(studentEntity.isActive()).isTrue();
         }, () -> Assertions.fail("Entity has not saved."));
+    }
+
+    @Test
+    void testIdempotencyWhileStudentRegistration() throws Exception {
+        var request = RegisterStudentCommand.builder()
+                .studentId(new StudentId("98989899"))
+                .grade("KG1")
+                .mobileNumber(new MobileNumber("+971555555555"))
+                .studentName("FirstName MiddleName LastName")
+                .schoolName("School Name")
+                .build();
+
+        //First call
+        mockMvc.perform(
+                        post("/v1/students")
+                                .contentType("application/json")
+                                .accept("application/json")
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andExpect(status().isCreated())
+        ;
+
+        //Second call
+        mockMvc.perform(
+                        post("/v1/students")
+                                .contentType("application/json")
+                                .accept("application/json")
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andExpect(status().isCreated())
+        ;
+
+        var entity = repository.findById(request.studentId().value());
+        Assertions.assertThat(entity.isPresent()).isTrue();
+    }
+
+
+    @Test
+    void badRequestWhileStudentRegistration() throws Exception {
+        var request = RegisterStudentCommand.builder()
+                .studentId(new StudentId("98989899"))
+                .grade("KG1")
+                .mobileNumber(new MobileNumber("+971555555555"))
+                .studentName("")
+                .schoolName("School Name")
+                .build();
+
+        //First call
+        mockMvc.perform(
+                        post("/v1/students")
+                                .contentType("application/json")
+                                .accept("application/json")
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andExpect(status().isBadRequest())
+        ;
+
+        var entity = repository.findById(request.studentId().value());
+        Assertions.assertThat(entity.isPresent()).isFalse();
     }
 
 }
